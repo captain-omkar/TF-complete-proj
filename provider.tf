039e77f33af34f2b30c6c41a0a7178cf739f18f1
captain-omkar <112746387+captain-omkar@users.noreply.github.com>
2023-02-03 11:08:57 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    
+  }
+
+  required_version = "~> 1.0"
+}
